e03c7c86c1c870fcd2a67d9864a26ce46444c309
hef <hef@pbrfrat.com>
2021-09-30 23:55:16 -0500

    looking at how to parse this thing

--- client/client.go
+++ client/client.go
@@ -2,10 +2,12 @@ package client
 
 import (
 	"crypto/tls"
+	"github.com/PuerkitoBio/goquery"
 	"go.uber.org/zap"
 	"golang.org/x/net/publicsuffix"
 	"net/http"
 	"net/http/cookiejar"
+	"net/url"
 )
 
 type Client struct {
@@ -41,4 +43,79 @@ func New(options ...Option) (*Client, error) {
 	}
 
 	return &c, nil
+}
+
+func (c *Client) newRequest(path string) (*http.Request, error) {
+
+	u, err := url.Parse(path)
+	if err != nil {
+		c.logger.Error("failed to parse path",
+			zap.String("path", path),
+			zap.Error(err),
+		)
+	}
+
+	csrfToken := c.csrfToken()
+	if csrfToken != "" {
+		q := u.Query()
+		q.Set("ct_"+csrfToken, "")
+		u.RawQuery = q.Encode()
+		// Encode adds a trailing "=", but we need to remove it
+		u.RawQuery = u.RawQuery[:len(u.RawQuery)-1]
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		c.logger.Error("error creating request",
+			zap.String("url", u.String()),
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	return req, nil
+}
+
+func (c *Client) do(req *http.Request) (*goquery.Document, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		c.logger.Error("error sending request",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		c.logger.Error("failed to parse response body",
+			zap.Error(err),
+		)
+	}
+
+	titleNode := doc.Find("title")
+	if titleNode.Text() == "Login" {
+		err = c.login()
+		if err != nil {
+			c.logger.Error("error logging in",
+				zap.Error(err),
+			)
+			return nil, err
+		}
+	}
+
+	req, err = c.newRequest(req.URL.String())
+	if err != nil {
+		c.logger.Error("error sending request",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	resp, err = c.client.Do(req)
+	if err != nil {
+		c.logger.Error("error sending request",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	doc, err = goquery.NewDocumentFromReader(resp.Body)
+	return doc, err
 }
--- client/status.go
+++ client/status.go
@@ -1,91 +1,16 @@
 package client
 
-import (
-	"github.com/PuerkitoBio/goquery"
-	"go.uber.org/zap"
-	"log"
-	"net/http"
-	"net/url"
-)
+import "log"
 
-func (c *Client) newRequest(path string) (*http.Request, error) {
-
-	u, err := url.Parse(path)
-	if err != nil {
-		c.logger.Error("failed to parse path",
-			zap.String("path", path),
-			zap.Error(err),
-		)
-	}
-
-	csrfToken := c.csrfToken()
-	if csrfToken != "" {
-		q := u.Query()
-		q.Set("ct_" + csrfToken, "")
-		u.RawQuery = q.Encode()
-		// Encode adds a trailing "=", but we need to remove it
-		u.RawQuery = u.RawQuery[:len(u.RawQuery)-1]
-	}
-
-	c.logger.Debug("csrf token",
-		zap.String("csrftoken", csrfToken),
-		zap.String("url", u.String()),
-	)
-
-	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
-	if err != nil {
-		c.logger.Error("error creating request",
-			zap.String("url", u.String()),
-			zap.Error(err),
-		)
-		return nil, err
-	}
-	return req, nil
-}
-
-func (c *Client) do(req *http.Request) (*goquery.Document, error) {
-	resp, err := c.client.Do(req)
-	if err != nil {
-		c.logger.Error("error sending request",
-			zap.Error(err),
-		)
-		return nil, err
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		c.logger.Error("failed to parse response body",
-			zap.Error(err),
-		)
-	}
-
-	titleNode := doc.Find("title")
-	if titleNode.Text() == "Login" {
-		err = c.login()
-		if err != nil {
-			c.logger.Error("error logging in",
-				zap.Error(err),
-			)
-			return nil, err
-		}
-	}
-
-	req, err = c.newRequest(req.URL.String())
-	if err != nil {
-		c.logger.Error("error sending request",
-			zap.Error(err),
-		)
-		return nil, err
-	}
-	resp, err = c.client.Do(req)
-	if err != nil {
-		c.logger.Error("error sending request",
-			zap.Error(err),
-		)
-		return nil, err
-	}
-	doc, err = goquery.NewDocumentFromReader(resp.Body)
-	return doc, err
+type DownstreamBoundedChannels struct {
+	ChannelId      string
+	LockStatus     string
+	Modulation     string
+	Frequency      string
+	Power          string
+	SnrSmr         string
+	Corrected      string
+	Uncorrectables string
 }
 
 func (c *Client) Status() (err error) {
@@ -99,6 +24,12 @@ func (c *Client) Status() (err error) {
 	if err != nil {
 		return err
 	}
+
+
+	// I really need xpath
+	//table := doc.Find("table >")
+
+	//Downstream Bonded Channels
 
 	html, _ := doc.Html()
 	log.Printf("%s", html)
